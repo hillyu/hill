24b444efdc75418020c6b3a32c630beb1ccbd0cf
hillyu <hillyu@live.com>
2019-04-19 11:34:59 +0000

    fix error caused by using sh shebang instead of bash, sh doesn't support (a b c) array literal

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,7 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 
 #install on-my-zsh
+dotpath=$1
 [ -z $@ ] && read -p "dotfile path: " dotpath
 cd $HOME
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
