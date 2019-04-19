123950fd0c52aae6c9edbf4ef99c3d912f9641c8
hillyu <hillyu@live.com>
2019-04-19 12:17:03 +0000

    add .zshenv to bootstrap filelist

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
 
-#install on-my-zsh
 dotpath=$1
 [ -z $@ ] && read -p "dotfile path: " dotpath
 cd $HOME
+#install on-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+#install zsh-custom plugins:
+git clone --recursive https://github.com/joel-porquet/zsh-dircolors-solarized \
+    $HOME/.oh-my-zsh/custom/plugins/zsh-dircolors-solarized
+git clone --recursive https://github.com/unixorn/warhol.plugin.zsh.git \
+    $HOME/.oh-my-zsh/custom/plugins/warhol
+git clone https://github.com/zdharma/fast-syntax-highlighting.git \
+    $HOME/.oh-my-zsh/custom/plugins/fast-syntax-highlighting
 # softlink dotfiles
-item_to_link=('.config' '.vimrc' '.zshrc' 'zshrc.mac' 'zshrc.linux' 'kitty.conf.mac')
+item_to_link=('.config' '.vimrc' '.zshrc' 'zshrc.mac' 'zshrc.linux' '.zshenv' 'kitty.conf.mac')
 for i in ${item_to_link[@]}; do
     [ -L $i ] && rm -v $i
     [ -e $i ] && mv $i $i.old
