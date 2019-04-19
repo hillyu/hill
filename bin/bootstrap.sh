3dad5a8e5d165f5ac7eb0b80afc49f8458588da6
hill <hill@viper>
2019-04-19 19:50:59 +0800

    added symlink check for bootstrap.sh

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -9,7 +9,8 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 # softlink dotfiles
 item_to_link=('.config' '.vimrc' '.zshrc' 'zshrc.mac' 'zshrc.linux' 'kitty.conf.mac')
 for i in ${item_to_link[@]}; do
-    [ -n $i ] && mv $i $i.old
+    [ -L $i ] && rm -v $i
+    [ -e $i ] && mv $i $i.old
     ln -vs $dotpath/$i ~/
 done
 
