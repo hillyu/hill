bf8e98d5ffd9282b570493ad555f90aacb2192d5
hill <hillyu@live.com>
2019-04-19 19:21:31 +0800

    Fix bug in bootstrap script

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -2,11 +2,13 @@
 
 
 #install on-my-zsh
-read -p "dotfile path: " $dotpath
+[ -z $@ ] && read -p "dotfile path: " dotpath
+cd $HOME
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 # softlink dotfiles
 item_to_link=('.config' '.vimrc' '.zshrc' 'zshrc.mac' 'zshrc.linux' 'kitty.conf.mac')
 for i in ${item_to_link[@]}; do
-    ln -s "$dotpath"/"$i" ~/
+    [ -n $i ] && mv $i $i.old
+    ln -vs $dotpath/$i ~/
 done
 
