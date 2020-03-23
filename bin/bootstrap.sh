5a1acb4de4de7b427deab5cb1a662a54bef8dea3
Hill <hillyu@live.com>
2020-03-24 00:17:05 +0800

    modify bootstrap

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -4,6 +4,14 @@
 dotpath=$1
 [ -z $@ ] && read -p "dotfile path: " dotpath
 cd $HOME
+# softlink dotfiles
+item_to_link=('.config' '.vimrc' '.zshrc' 'zshrc.mac' 'zshrc.linux' '.zshenv' 'kitty.conf.mac' '.tmux.conf')
+for i in ${item_to_link[@]}; do
+    [ -L $i ] && rm -v $i
+    [ -e $i ] && mv $i $i.old
+    ln -vs $dotpath/$i ~/
+done
+
 #install on-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 #install zsh-custom plugins:
@@ -13,11 +21,3 @@ git clone --recursive https://github.com/unixorn/warhol.plugin.zsh.git \
     $HOME/.oh-my-zsh/custom/plugins/warhol
 git clone https://github.com/zdharma/fast-syntax-highlighting.git \
     $HOME/.oh-my-zsh/custom/plugins/fast-syntax-highlighting
-# softlink dotfiles
-item_to_link=('.config' '.vimrc' '.zshrc' 'zshrc.mac' 'zshrc.linux' '.zshenv' 'kitty.conf.mac' '.tmux.conf')
-for i in ${item_to_link[@]}; do
-    [ -L $i ] && rm -v $i
-    [ -e $i ] && mv $i $i.old
-    ln -vs $dotpath/$i ~/
-done
-
