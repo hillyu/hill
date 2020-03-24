37bf655a665ac2ec00785a6a0712c32139e30125
Hill <hillyu@live.com>
2020-03-24 15:38:46 +0800

    fix omzsh install order

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -4,13 +4,6 @@
 dotpath=$1
 [ -z $@ ] && read -p "dotfile path: " dotpath
 cd $HOME
-# softlink dotfiles
-item_to_link=('.config' '.vimrc' '.zshrc' 'zshrc.mac' 'zshrc.linux' '.zshenv' 'kitty.conf.mac' '.tmux.conf')
-for i in ${item_to_link[@]}; do
-    [ -L $i ] && rm -v $i
-    [ -e $i ] && mv $i $i.old
-    ln -vs $dotpath/$i ~/
-done
 
 #install on-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
@@ -21,3 +14,11 @@ git clone --recursive https://github.com/unixorn/warhol.plugin.zsh.git \
     $HOME/.oh-my-zsh/custom/plugins/warhol
 git clone https://github.com/zdharma/fast-syntax-highlighting.git \
     $HOME/.oh-my-zsh/custom/plugins/fast-syntax-highlighting
+
+# softlink dotfiles
+item_to_link=('.config' '.vimrc' '.zshrc' 'zshrc.mac' 'zshrc.linux' '.zshenv' 'kitty.conf.mac' '.tmux.conf')
+for i in ${item_to_link[@]}; do
+    [ -L $i ] && rm -v $i
+    [ -e $i ] && mv $i $i.old
+    ln -vs $dotpath/$i ~/
+done
