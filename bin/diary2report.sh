53e71fe514028044da446e118d7c88a051f3f897
Hill <hillyu@live.com>
2020-05-06 19:16:13 +0800

    update

--- bin/diary2report.sh
+++ bin/diary2report.sh
@@ -1,11 +1,11 @@
 data=`<"${1:-/dev/stdin}"`
 echo "$data"|awk -F '|' '
-$3 !="" {i++; s+=$4; cat[i]=$2;title[i]=$3;time[i]=$4;status[i]=$5}
-$5 !="" && $5 !~ "Done" {e++;todo[e] ="[" $2 "] " $3}
+$4 ~ "[[:digit:]]h"  { i++; s+=$4; cat[i]=$2;title[i]=$3;time[i]=$4;status[i]=$5}
+$2 !="" && $5 !~ "one" {e++;todo[e] ="[" $2 "] " $3}
 END {
 print "Update:"
 for (d=1;d<i; ++d) {
-    percent=int(time[i]*100/s)
+    percent=int(time[d]*100/s)
     acc+=percent
     printf ("[%s] %s (%d%% - %s)\n",cat[d],title[d],percent,status[d]~"one"?"Done":"WIP")
 }
