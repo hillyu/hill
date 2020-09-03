a6f4562f7686cc5f4bfa9883abf83bc2717ceba4
Hill <hillyu@live.com>
2020-09-03 09:21:53 +0800

    abunch of update

--- bin/amdClock.sh
+++ bin/amdClock.sh
@@ -8,10 +8,11 @@
 # Xin pro official
 #ryzenadj -a 35000 -b 50000 -c 42000 -f 80
 # lowest should be 15000, lower than that will cause lag 
-ryzenadj -a 15000 -b 25000 -c 15000 -d 2 -f 80
+ryzenadj -a 15000 -b 42000 -c 15000 -d 2 -f 80
 # set power to low on GPU to prevent over use of power from vega 
 #/home/hill/bin/chicony-ir-toggle on
 #echo "low" > /sys/class/drm/card0/device/power_dpm_force_performance_level
 # default is 24 which is too slow for my mx master (it uses 6 as min latency)
 #echo 6 > /sys/kernel/debug/bluetooth/hci0/conn_min_interval
 echo 20000 > /sys/bus/usb/devices/3-1/power/autosuspend_delay_ms
+#echo 15 > /proc/sys/vm/swappiness
