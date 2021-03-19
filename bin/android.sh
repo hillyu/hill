e545517b287e7deda2924b3c2895431c2047d094
Hill <hillyu@live.com>
2021-03-19 16:45:01 +0800

    local update

--- bin/android.sh
+++ bin/android.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-qemu-system-x86_64 -enable-kvm -machine q35 -device intel-iommu -vga virtio -display sdl,gl=on \
+qemu-system-x86_64 -enable-kvm -machine q35 -vga virtio -display gtk,gl=on \
                    -usb -device usb-tablet \
-                   -device virtio-mouse-pci -device virtio-keyboard-pci \
-                   -m 2048 -smp 2 -cpu host \
+                   -m 8G -smp 4 -cpu host \
                    -soundhw ac97 \
                    -net nic,model=e1000 -net user \
                    -monitor stdio vms/android.img
+                   # -device virtio-mouse-pci -device virtio-keyboard-pci -device intel\
