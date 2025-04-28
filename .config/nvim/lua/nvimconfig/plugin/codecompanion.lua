b9caff9c12dd4ebad074f356c5aa50876135d3d6
Hill <xcyu@google.com>
2025-04-29 00:55:53 +0800

    nerdfont, neovim config

--- .config/nvim/lua/nvimconfig/plugin/codecompanion.lua
+++ .config/nvim/lua/nvimconfig/plugin/codecompanion.lua
@@ -4,34 +4,32 @@ return {
         "nvim-lua/plenary.nvim",
         "nvim-treesitter/nvim-treesitter",
     },
-    config = function()
-        require("codecompanion").setup({
-            strategies = {
-                chat = {
-                    adapter = "litellm",
-                },
-                inline = {
-                    adapter = "litellm",
-                },
-                cmd = {
-                    adapter = "litellm",
-                },
+    opts= {
+        strategies = {
+            chat = {
+                adapter = "litellm",
             },
-            adapters = {
-                litellm = function()
-                    return require("codecompanion.adapters").extend("openai_compatible", {
-                        schema = {
-                            model = {
-                                default = "gemini-2.5-pro",
-                            },
-                        },
-                        env = {
-                            url = "http://g.ps.ai:12000",
-                            api_key = "hillyu",
-                        },
-                    })
-                end,
+            inline = {
+                adapter = "litellm",
             },
-        })
-    end,
+            cmd = {
+                adapter = "litellm",
+            },
+        },
+        adapters = {
+            litellm = function()
+                return require("codecompanion.adapters").extend("openai_compatible", {
+                    schema = {
+                        model = {
+                            default = "gemini-2.5-pro",
+                        },
+                    },
+                    env = {
+                        url = "http://g.ps.ai:12000",
+                        api_key = "hillyu",
+                    },
+                })
+            end,
+        },
+    }
 }
--- .config/nvim/lua/nvimconfig/plugin/fugitive.lua
+++ .config/nvim/lua/nvimconfig/plugin/fugitive.lua
@@ -1,10 +1,3 @@
 return {
     'tpope/vim-fugitive',
-    -- config = function()
-      -- vim.o.background = 'dark' -- or 'light'
-      -- require('solarized').setup({
-          -- transparent = true, -- enable transparent background
-      -- })
-      -- vim.cmd.colorscheme 'solarized'
-    -- end,
   }
