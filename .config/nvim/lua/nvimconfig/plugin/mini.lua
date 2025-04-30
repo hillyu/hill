6b921e34501dc149051e35544ee339aaf8f0625a
Hill <xcyu@google.com>
2025-04-30 01:57:03 +0000

    update mini.nvim

--- .config/nvim/lua/nvimconfig/plugin/mini.lua
+++ .config/nvim/lua/nvimconfig/plugin/mini.lua
@@ -2,5 +2,39 @@ return {
     { 'echasnovski/mini.indentscope', version = false },
     { 'echasnovski/mini.trailspace', version = false },
     { 'echasnovski/mini.icons', version = false },
+    { 'echasnovski/mini.clue', version = false, opts = {
+        triggers = {
+            -- Leader triggers
+            { mode = 'n', keys = '<Leader>' },
+            { mode = 'x', keys = '<Leader>' },
+
+            -- Built-in completion
+            { mode = 'i', keys = '<C-x>' },
+
+            -- `g` key
+            { mode = 'n', keys = 'g' },
+            { mode = 'x', keys = 'g' },
+
+            -- Marks
+            { mode = 'n', keys = "'" },
+            { mode = 'n', keys = '`' },
+            { mode = 'x', keys = "'" },
+            { mode = 'x', keys = '`' },
+
+            -- Registers
+            { mode = 'n', keys = '"' },
+            { mode = 'x', keys = '"' },
+            { mode = 'i', keys = '<C-r>' },
+            { mode = 'c', keys = '<C-r>' },
+
+            -- Window commands
+            { mode = 'n', keys = '<C-w>' },
+
+            -- `z` key
+            { mode = 'n', keys = 'z' },
+            { mode = 'x', keys = 'z' },
+        },
+    }
+    },
 }
 
